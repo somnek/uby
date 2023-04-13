57d7da689992cd6f0bc5325500b0e505fa5eab9e
Som Nek <38861036+somnek@users.noreply.github.com>
2023-04-14 01:07:41 +0800

    scrape dependencies 1st page

--- scraper/main.go
+++ scraper/main.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/gocolly/colly"
 )
 
@@ -14,13 +15,17 @@ func toNum(s string) int {
 }
 
 type Deps struct {
-	name string
+	user   string
+	repo   string
+	avatar string
+	url    string
 }
 
-func scrapeRepoCount(url string) int {
-	c := colly.NewCollector()
+func scrape(c *colly.Collector, url string) int {
 
+	// ----------------------------------------------
 	var count int
+
 	c.OnHTML("a.btn-link.selected", func(e *colly.HTMLElement) {
 		lines := strings.Split(e.Text, "\n")
 		for i, line := range lines {
@@ -31,30 +36,36 @@ func scrapeRepoCount(url string) int {
 		}
 	})
 
+	// ----------------------------------------------
+	dependents := []Deps{}
+
+	c.OnHTML("div.Box-row", func(e *colly.HTMLElement) {
+		avatar := e.ChildAttr("img", "src")
+		user := e.ChildAttrs("a", "href")[0][1:]
+		repo := e.ChildText("a.text-bold")
+		url := fmt.Sprintf("https://github.com/%s/%s", user, repo)
+		dependents = append(dependents, Deps{
+			user:   user,
+			repo:   repo,
+			avatar: avatar,
+			url:    url,
+		})
+	})
+
+	// ----------------------------------------------
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
 	c.Visit(url)
-	fmt.Println(count)
+
+	fmt.Println("count:", count)
+	spew.Dump(dependents)
 	return count
 }
 
-func scrapeDependents(repoCount int) []Deps {
-	hasNextPage := false
-	dependents := []Deps{}
-
-	for hasNextPage {
-		dependents = append(dependents, Deps{name: "test"})
-		break
-	}
-	return dependents
-}
-
 func main() {
+	c := colly.NewCollector()
 	url := "https://github.com/aquasecurity/trivy/network/dependents"
-	repoCount := scrapeRepoCount(url)
-	dependents := scrapeDependents(repoCount)
-	_ = dependents
-
+	scrape(c, url)
 }
